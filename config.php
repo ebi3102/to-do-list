8be0b7300e583dad619addb15d5aa45a00c6a67d
Ebrahim Moeini <padiabcom@gmail.com>
2018-09-03 00:01:20 +0430

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
-<?php 
+<?php
+
+// ** MySQL settings ** // 
 
 $dbConfig  = array(
-	'hostname' => 'localhost' ,
-	'username' => 'root' ,
-	'password' => '' ,
-	'db_name' => 'to-do_db'
+	'hostname' => 'localhost' , 	// MySQL Hostname
+	'username' => 'root' ,		// MySQL database username
+	'password' => '' ,		// MySQL database password
+	'db_name' => 'to-do_db'		// MySQL hostname
 );
  ?>
